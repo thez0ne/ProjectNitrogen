646e340a61dfdeda1137ff1b0ebbe5afc29f3fc0
neoJINXD <neojinxd@gmail.com>
2021-01-06 03:03:19 -0500

    For development FPS Counter UI Script

---
+++ Unity/Scripts/Utils/FPSCounter.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+// Author: Anik Patel
+// Description: Basic class for use as a UI FPS counter
+// Version: 1.0
+// Changes: [N/A]
+// WARNING Only use this script in development, the console with give NullReferenceErrors but will still work
+public class FPSCounter : MonoBehaviour
+{
+    private int fps;
+
+    // Boolean for if using TMPro or the base Unity UI
+    [SerializeField] private bool usingTMP = false;
+
+    // Refences to the Text Components
+    [SerializeField] private Text fpsText = null;
+    [SerializeField] private TMP_Text fpsTextTMP;
+
+    void Update()
+    {
+        fps = (int) (1f / Time.unscaledDeltaTime);
+        if (usingTMP)
+        {
+            fpsTextTMP.text = $"{fps} fps";
+        }
+        else
+        {
+            fpsText.text = $"{fps} fps";
+        }
+        // Debug.Log($"Running at {fps}");
+    }
+}
